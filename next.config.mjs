2ccdf1a497aaa64a4f0c1d585fe2706728150c48
asfarwaheed01 <asfarwaheedch123@gmail.com>
2024-10-23 12:00:25 +0500

    heygen api changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,25 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    async rewrites() {
-      return [
-          {
-              source: '/api/get-access-token',
-              destination: 'https://main.dal9auz5q4mbe.amplifyapp.com/api/get-access-token',
-          }
-      ];
-  },
+    env: {
+      HEYGENAPIKEY: process.env.HEYGENAPIKEY
+    },
   async headers() {
-      return [
-          {
-              source: '/api/:path*',
-              headers: [
-                  { key: 'Access-Control-Allow-Origin', value: '*' },
-                  { key: 'Access-Control-Allow-Methods', value: 'GET,POST,OPTIONS' },
-                  { key: 'Access-Control-Allow-Headers', value: 'Content-Type,Authorization,x-api-key' },
-              ],
-          },
-      ];
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET,POST,OPTIONS' },
+          { key: 'Access-Control-Allow-Headers', value: 'Content-Type,Authorization,x-api-key' },
+        ],
+      },
+    ];
   },
 };
 
